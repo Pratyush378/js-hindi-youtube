6e5bff7c2e9a84e500e357c1e762e65bd7415dca
Pratyush378 <pratyush6566@gmail.com>
2024-08-25 19:26:34 +0000

    strings

---
+++ .devcontainer/basics/05_strings.js
@@ -0,0 +1,26 @@
+const name="hitesh"
+const repocount=50
+console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+
+const gamename = new String("pratyush-kuma") // store like an array of characters check inspect on chrome
+// console.log(gamename[0]);
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf("a"));
+
+// const newString = gamename.substring(0,4) // check slice js mdn on chrome for diff b/w substring ans slice
+// console.log(newString);
+// const newStringone = gamename.slice(-6)  // can also print from backwards
+// console.log(newStringone);
+
+const newstringone="     hello   "
+console.log(newstringone);
+console.log(newstringone.trim());  // to trim any extra space
+
+const url="https://pratyush.com%20bh"
+console.log(url.replace("%20","-"));
+console.log(gamename.split("-"));
+
+
