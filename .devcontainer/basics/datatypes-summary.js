ea053c167c241c47759f1fa13ab6e37b0c15f39e
Pratyush378 <pratyush6566@gmail.com>
2024-08-25 04:45:16 +0000

    datatypes summary

---
+++ .devcontainer/basics/datatypes-summary.js
@@ -0,0 +1,25 @@
+/*
+Datatypes can be classified into 2 types:
+1) Primitive: string,boolean,null,number,undefined,symbol,BigInt 
+
+const score=100 //number
+const isLoggedIn=false //boolean
+let userEmail /// undefined
+const temp=null
+const id = Symbol('123')  // unique symbol
+const bignumber = 32234354356547686846n 
+*/
+/*
+2) Reference: Array,Objects,Functions
+
+const heros = {"shaktiman","logan","peter"}
+let myObj = {
+    name:"avi",
+    age=22,
+    }
+*/
+const myFunction =function(){
+   console.log("Hello World");
+}
+
+
