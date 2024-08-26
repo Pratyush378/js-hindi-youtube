02203c0738efbe55a0e2881566713a8bd4465d74
Pratyush378 <pratyush6566@gmail.com>
2024-08-26 17:57:18 +0000

    array

---
+++ .devcontainer/02_basics/01_Array.js
@@ -0,0 +1,27 @@
+const myArr = [1,2,3,4,5]
+const hero = ["logan","IRon","Spiderman"]
+//console.log(myArr[1]);
+// console.log(myArr);
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+// console.log(myArr.indexOf(4)); // 3
+
+
+//const arr = new Array(1,2,3,4)
+//console.log(arr);
+
+// Slice ,  splice
+
+console.log("A ",myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+
+console.log("B ",myArr);
+const myn2 = myArr.splice(1,3)  // check difference b/w both on mdn
+console.log(myn2);
+
+
